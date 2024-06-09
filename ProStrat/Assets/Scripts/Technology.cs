a33f063ce5d200fc1c42f3edbb429d8b5bae77fe
Nexix <109114492+Nexix59@users.noreply.github.com>
2024-06-08 22:25:31 -0700

    hgfd
    ujhgf

---
+++ ProStrat/Assets/Scripts/Technology.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Technology
+{
+    public string name;
+    public bool isResearched;
+    public int researchCost;
+    public List<Technology> prerequisites;
+
+    // Constructor and methods for researching technologies
+}
